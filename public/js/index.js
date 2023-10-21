3fc38e5e3a870ee3e6b5d6b6cbc9ff34e9c04e9e
Joaquin Puente <joaquinpuentecoronel@gmail.com>
2023-10-21 00:47:22 -0300

    Chat con  WebSocket

---
+++ public/js/index.js
@@ -0,0 +1,62 @@
+(function () {
+    const socket = io();
+    let username;
+
+    //form-message
+    const formMessage = document.getElementById('form-message');
+    //input-message
+    const inputMessage = document.getElementById('input-message');
+    //log-message
+    const logMessage = document.getElementById('log-message');
+
+    formMessage.addEventListener('submit', (event)=>{
+        event.preventDefault();
+
+        const text = inputMessage.value;
+        socket.emit('new-message', {username,text})
+        inputMessage.value ='';
+        inputMessage.focus();
+
+    })
+
+    function updateLogMessage(message){
+        logMessage.innerText='';
+        message.forEach((msg)=>{
+            const p = document.createElement('p');
+            p.innerHTML = `${msg.username}: ${msg.text}`
+            logMessage.appendChild(p);
+        })
+    }
+
+    socket.on('notification', ({message}) => {
+        updateLogMessage(message);
+    })
+
+    socket.on('new-client', () => {
+        Swal.fire({
+            text: 'Nuevo usuario conectado',
+            toast: true,
+            position: 'top-right',
+        });
+    });
+
+    Swal.fire({
+        title: 'Identificate por favor',
+        input: 'text',
+        inputLabel: 'Ingresa tu username',
+        allowOutsideClick: false,
+        inputValidator: (value)=>{
+            if(!value){
+                return 'Necesitamos que ingreses un username para continuar';
+            }
+        }
+    })
+    .then((result) => {
+        username = result.value.trim();
+        console.log(`Hola ${username}`);
+    }).catch((err) => {
+        console.log(`No se obstuvo username - Error: ${err}`);
+    });
+
+
+})();
--- src/socket.js
+++ src/socket.js
@@ -1,8 +1,29 @@
 import { Server } from "socket.io";
 
-let socketServer;
+let io;
+let message = [];
 
 export const init = (httpServer)=>{
-    socketServer = new Server(httpServer);
+    io = new Server(httpServer);
+
+
+    io.on('connection', (socketClient)=>{
+        console.log('connection');
+        console.log(`Se ha conectado el cliente ${socketClient.id}`);
+    
+        socketClient.emit('notification', {message})
+
+        socketClient.broadcast.emit('new-client')
+
+        socketClient.on('new-message', (data)=>{
+            const { username, text } = data;
+            message.push({username,text});
+            io.emit('notification', {message})
+        })
+    
+    })
+
+
+    console.log('Server socket running...');
     
 }
